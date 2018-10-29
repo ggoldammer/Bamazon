8d2d7fbe85dc387243ab57d7d384477adac7c7ee
Gabriel Viveros-Goldammer <gabrielgoldammer@gmail.com>
2018-10-29 00:51:27 -0700

    Made fixes to make inquirer work. Needs improvement

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,33 +8,69 @@ let connection = mySql.createConnection({
     database: "bamazon_db"
 });
 
-connection.connect(function(err){
-    if (err){
+connection.connect(function (err) {
+    if (err) {
         console.log("Error: " + err);
         return;
     }
     console.log("Connected as ID " + connection.threadId);
 });
 
-connection.query("SELECT product_name FROM products", function(err, results){
-    let resultsArray = [];
+connection.query("SELECT product_id, product_name, stock_quantity FROM products", function (err, results) {
+    let productNames = [];
+    let productID;
     for (let i = 0; i < results.length; i++) {
-        resultsArray.push(results[i].product_name);
+        productNames.push(results[i].product_name);
     }
-    console.log(resultsArray);
-    initiateInquirer(resultsArray);
+    initiateQuestions(productNames, results);
 });
 
 connection.end();
 
-function initiateInquirer(productNames){
-    inquirer.prompt([{
+function initiateQuestions(productNames, results) {
+    inquirer.prompt(questions(productNames)[0]).then(answer1 => {
+        inquirer.prompt(questions(productQuantity(answer1, results))[1]).then(answer2 => {
+            console.log(answer1);
+            console.log(answer2);
+        });
+    });
+}
+
+let questions = function (input) {
+    let questionArray = [{
         type: "list",
         name: "productChoice",
         message: "Which product would you like to purchase?",
-        choices: productNames
+        choices: input
+    },
+    {
+        type: "input",
+        name: "purchaseAmount",
+        message: "How many would you like to purchase? (Current Quantity: " + input + ")"
+    }];
+
+    return questionArray;
+}
+
+let productQuantity = function (productName, productArray) {
+    for (let i = 0; i < productArray.length; i++) {
+        if (productName.productChoice === productArray[i].product_name) {
+            if(productArray[i].stock_quantity > 0){
+                productId = .indexOf(productName.productChoice);
+                console.log(productId);
+                return productArray[i].stock_quantity;
+            } else{
+                let response = "SOLD OUT";
+                return response;
+            }
+        }
     }
-    ]).then(answers => {
-    
-    });
+}
+
+function productPurchase(productName, productQuantity){
+    if(productQuantity === "SOLD OUT"){
+        console.log("Insufficient inventory");
+    } else {
+        connection.query("")
+    }
 }
