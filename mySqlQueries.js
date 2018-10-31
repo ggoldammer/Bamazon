61e5a49f0e0d1b214a5f9414897e506c18a401f0
Gabriel Viveros-Goldammer <gabrielgoldammer@gmail.com>
2018-10-30 18:34:38 -0700

    Added Query callback function.

--- mySqlQueries.js
+++ mySqlQueries.js
@@ -1,14 +1,15 @@
 const mySqlConnect = require("./mySqlConnect");
 
-let products = getProducts(results);
-
-console.log(products);
-
 function getProducts() {
     mySqlConnect.connection.query("SELECT product_id, product_name, stock_quantity FROM products", function (err, results) {
-        return results;
+        return products(results);
     });
 }
+
+function products(results){
+    console.log(results);
+    return results;
+};
 
 module.exports = {
     products,
